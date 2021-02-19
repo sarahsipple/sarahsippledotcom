9efd2c14b43c179b594afc0e5e00b93f49b820f1
Sarah Sipple <hello@sarahsipple.com>
2021-02-19 15:29:34 -0500

    added logos, added js for hover effect, layout

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const card = document.querySelector('.card-body');
+
+function hoverOver() {
+    card.classList.remove('hidden');
+}
+
+function hoverOut() {
+    card.classList.add('hidden');
+}
